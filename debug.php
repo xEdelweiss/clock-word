1ec1289d09ce830eb89007e2d6f9ed73a7e21a80
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-01-28 17:02:00 +0200

    Update debug file (stringify all minutes of day)

--- debug.php
+++ debug.php
@@ -2,7 +2,12 @@
 
 require 'vendor/autoload.php';
 
+$time = \Carbon\Carbon::createFromFormat('H:i', '0:00');
 $generator = new \xEdelweiss\ClockWord\Generator();
-$time = \Carbon\Carbon::createFromFormat('H:i', '17:48');
 
-echo $generator->stringifyTime($time); // пять часов сорок восемь минут вечера
+for ($i = 0; $i < 24*60; $i++) {
+    echo $time->format('H:i') . PHP_EOL;
+    echo ' - ' . $generator->stringifyTime($time) . PHP_EOL;
+    echo ' - ' . $generator->stringifyTime($time, \xEdelweiss\ClockWord\Generator::TIME_FORMAT_24) . PHP_EOL;
+    $time->addMinute();
+}
