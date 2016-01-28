e69f516ac17feada164925bb058b47b617bc291d
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-01-28 16:43:48 +0200

    Update Readme

--- debug.php
+++ debug.php
@@ -3,4 +3,4 @@
 require 'vendor/autoload.php';
 
 $g = new \xEdelweiss\ClockWord\Generator();
-dump($g->stringifyTime(\Carbon\Carbon::createFromFormat('H:i', '13:30')));
+dump($g->stringifyTime(\Carbon\Carbon::createFromFormat('H:i', '17:48')));
