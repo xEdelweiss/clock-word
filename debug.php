0540aa4a839b1c443d41e28a6e81d19189bc1ca3
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-01-28 17:02:48 +0200

    Update debug file (+1 one more)

--- debug.php
+++ debug.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 $time = \Carbon\Carbon::createFromFormat('H:i', '0:00');
 $generator = new \xEdelweiss\ClockWord\Generator();
 
-for ($i = 0; $i < 24*60; $i++) {
+for ($i = 0; $i <= 24*60; $i++) {
     echo $time->format('H:i') . PHP_EOL;
     echo ' - ' . $generator->stringifyTime($time) . PHP_EOL;
     echo ' - ' . $generator->stringifyTime($time, \xEdelweiss\ClockWord\Generator::TIME_FORMAT_24) . PHP_EOL;
