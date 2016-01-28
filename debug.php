765d05618516bdb847740b177d18648cea2bead9
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-01-28 16:44:37 +0200

    Update debug file

--- debug.php
+++ debug.php
@@ -2,5 +2,7 @@
 
 require 'vendor/autoload.php';
 
-$g = new \xEdelweiss\ClockWord\Generator();
-dump($g->stringifyTime(\Carbon\Carbon::createFromFormat('H:i', '17:48')));
+$generator = new \xEdelweiss\ClockWord\Generator();
+$time = \Carbon\Carbon::createFromFormat('H:i', '17:48');
+
+echo $generator->stringifyTime($time); // пять часов сорок восемь минут вечера
